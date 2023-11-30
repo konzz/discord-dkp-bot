74e7b8f513b2337b6434518f4c52388bbfd10a19
A happy cat <acasadotorres@gmail.com>
2023-11-30 18:33:05 +0100

    updated db connection

--- db.js
+++ db.js
@@ -1,12 +1,5 @@
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient(process.env.MONGO_URL, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
+const client = new MongoClient(process.env.MONGO_URL);
 
 module.exports = client;

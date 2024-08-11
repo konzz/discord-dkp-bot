737267ebac936e48b459342dae50857861d42699
A happy cat <acasadotorres@gmail.com>
2024-08-12 00:54:14 +0200

    added url to items

--- commands/dkphistory.js
+++ commands/dkphistory.js
@@ -16,19 +16,17 @@ module.exports = {
         const log = player.log.sort((a, b) => b.date - a.date)
             .map((e, index, entries) => {
                 if (e.comment === 'Tick' && entries[index + 1]?.comment == 'Tick' && entries[index + 1]?.raid._id.toString() === e.raid._id.toString()) {
-                    console.log('Tick increase!');
                     ticks++;
                     raid = e.raid;
                     return;
                 }
                 if (e.comment === 'Tick') {
-                    console.log('Tick increase! final');
                     ticks++;
                     const message = `- <t:${Math.floor(e.date / 1000)}:d>  **${ticks}**${e.raid ? ` *${e.raid.name}* ` : ' '}*aggregated ticks*`
                     ticks = 0;
                     return message;
                 }
-                return `- <t:${Math.floor(e.date / 1000)}:d>  **${e.dkp}**${e.raid ? ` *${e.raid.name}* ` : ' '}*${e.comment}*`;
+                return `- <t:${Math.floor(e.date / 1000)}:d>  **${e.dkp}**${e.raid ? ` *${e.raid.name}* ` : ' '} ${e.item ? `[${e.item.name}](${e.item.url})` : `*${e.comment}*`}`;
             }).filter(e => e);
         if (log.length < 40) {
             await interaction.editReply({ content: log.join('\n'), ephemeral: true });

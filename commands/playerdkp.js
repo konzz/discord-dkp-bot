03d85e0f45aa8b763f0b2f51b52f7cc2ee23344a
A happy cat <acasadotorres@gmail.com>
2025-04-14 23:36:43 +0200

    fixed possible bug in playerdkp

--- commands/playerdkp.js
+++ commands/playerdkp.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription('Shows the DKP of a player')
         .addUserOption(option => option.setName('player').setDescription('The player').setRequired(false)),
     async execute(interaction, manager) {
-        interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ ephemeral: true });
         const guild = interaction.guild.id;
         const player = interaction.options.getUser('player') || interaction.user;
 
